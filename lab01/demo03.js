57fb089287154b7f8bff0772e7e7255d2180ea87
Tram Pham <nguyettram10@gmail.com>
2022-02-12 19:26:39 +0700

    feature(lab01): ES - convert data-type

---
+++ lab01/demo03.js
@@ -0,0 +1,19 @@
+/**
+ * Chuyển đổi kiểu dữ liệu cho biến
+ */
+
+let number = 100;
+console.log("number =", number);
+
+number = "Hello";
+console.log("number =", number);
+
+//Chuyển đổi chuỗi (string) thành số nguyên (integer)
+number = parseInt("123.45");
+console.log("(int) number =", number);
+
+//Chuyển đổi chuỗi (string) thành số thực (float)
+number = parseFloat("123.45");
+console.log("(float) number =", number);
+
+//
