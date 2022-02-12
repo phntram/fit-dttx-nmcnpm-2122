fadf9345a872cc4f49c66a19999873468ebfd102
Tram Pham <nguyettram10@gmail.com>
2022-02-12 20:05:42 +0700

    refactor(lab01): ES - Operator

--- lab01/demo05.js
+++ lab01/demo05.js
@@ -1,5 +1,5 @@
 /**
- * Các phếp toán và phép so sánh
+ * Các phếp toán
  */
 
 let num = 100;
@@ -11,6 +11,7 @@ console.log("num = num / 3 =>", num / 3);
 console.log("num = num % 3 =>", num % 3);
 console.log("num = num / 3 =>", parseInt(num / 3));
 
+num = 100;
 console.log("num =", num);
 console.log("num++ =>", num++);
 console.log("num =", num);
@@ -18,3 +19,13 @@ console.log("++num =>", ++num);
 console.log("num-- =>", num--);
 console.log("num =", num);
 console.log("--num =>", --num);
+
+num = 100;
+num += 5;
+console.log("num += 5 =>", num);
+num -= 4;
+console.log("num -= 4 =>", num);
+num *= 2;
+console.log("num *= 2 =>", num);
+num /= 6;
+console.log("num /= 6 =>", num);
