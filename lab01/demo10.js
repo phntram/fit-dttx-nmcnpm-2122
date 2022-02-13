c6e2b35a473a7c64f7bd185d6b34024c35315719
Tram Pham <nguyettram10@gmail.com>
2022-02-13 10:51:33 +0700

    feature(lab01): ES - Array

---
+++ lab01/demo10.js
@@ -0,0 +1,34 @@
+/**
+ * Array trong ES
+ */
+
+'use strict'
+
+console.clear();
+
+//Khai báo mảng 1 chiều (array)
+let a = [1, 2, 3, 4, 5]
+
+console.log("Print array");
+for(let i = 0; i < a.length; i++){
+    console.log(a[i]);
+}
+console.log(a);
+
+console.log("Print array with arrayObject method");
+a.forEach(item => {
+    console.log(item);
+})
+
+console.log("Revert array");
+a.reverse();
+console.log(a);
+a.reverse();
+
+console.log("Insert new item to array");
+a.push(99);
+console.log(a);
+
+console.log("Remove item in array");
+a.splice(2, 1)
+console.log(a);
